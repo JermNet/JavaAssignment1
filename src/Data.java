4e3e445721adbe6b18ffb521b0d45d5e600f00d7
Jeremy <jeremywhitenect@gmail.com>
2025-01-10 14:00:38 -0400

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,25 @@
+public class Data {
+
+    public static void main(String[] args) {
+        byte sampleByte = 10;
+        short sampleShort = 20;
+        int sampleInt = 30;
+        long sampleLong = 40;
+        float sampleFloat = 10.5f;
+        char sampleChar = 'J';
+        boolean sampleBoolean = true;
+        double doubleOne = 15.2, doubleTwo = 20.3;
+
+        System.out.println("Here are some sample values:\n"
+                + "Byte: " + sampleByte + "\n"
+                + "Short: " + sampleShort + "\n"
+                + "Int: " + sampleInt + "\n"
+                + "Long: " + sampleLong + "\n"
+                + "Float: " + sampleFloat + "\n"
+                + "Char: " + sampleChar + "\n"
+                + "Boolean: " + sampleBoolean + "\n"
+                + "The area of the rectangle with sides " + doubleOne + " and " + doubleTwo + " is: " + doubleOne*doubleTwo);
+
+    }
+
+}
---
+++ src/Escape.java
@@ -0,0 +1,12 @@
+public class Escape {
+
+    public static void main(String[] args) {
+        System.out.println("\"Java Journey\""
+                + "\n---------------"
+                + "\n\"Every Error is a \\nnew learning opportunity!\""
+                + "\n\t- A Budding Programmer"
+                + "\n---------------");
+
+    }
+
+}
